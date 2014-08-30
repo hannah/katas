94a528c1c4ea8952feb03b4cbcac4fd49ce654f5
Hannah Zabarsky <h.zabarsky@gmail.com>
2014-08-29 17:17:46 -0700

    outlined merge sort with errors

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,16 +1,25 @@
-# P rinceton pseudocode - http://www.princeton.edu/~achaney/tmve/wiki100k/docs/Merge_sort.html
-# function merge_sort(m)
-#     if length(m) ≤ 1
-#         return m
-#     var list left, right, result
+def merge_sort(array)
+  if array.length == 1
+    return array
+  else
+    mid = (array.length)/2
+    left = array.slice(0, mid)
+    right = array.slice(mid, -1)
 
+    merge(merge_sort(left), merge_sort(right))
+  end
+end
 
-#     var integer middle = length(m) / 2
-#     for each x in m up to middle
-#          add x to left
-#     for each x in m after middle
-#          add x to right
-#     left = merge_sort(left)
-#     right = merge_sort(right)
-#     result = merge(left, right)
-#     return result
+def merge(left, right)
+  sorted = []
+  until left.empty? or right.empty?
+    if left[0] <= right[0]
+      sorted << left.shift
+    else
+      sorted << right.shift
+    end
+  end
+end
+
+list = [1, 3, 6, 7, 3, 6, 5, 7, 2, 9, 0]
+merge_sort(list)
